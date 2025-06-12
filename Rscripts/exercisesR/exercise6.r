3e448946c81f3c7c819ef0b7b641fdc881e1edf7
JoseLastra <jose.lastra@pucv.cl>
2025-06-12 10:18:01 +0200

    changes 1

--- Rscripts/exercisesR/exercise6.r
+++ Rscripts/exercisesR/exercise6.r
@@ -15,20 +15,23 @@ nitrogen <- c(34.98, 40.89, 42.07, 37.18, 37.99, 34.89, 41.22, 46.69, 49.42, 45.
 library(emmeans)
 
 b.fixed <- lm(nitrogen ~ block + trt, data=rcbd)
-anova(b.fixed)
+# Use anova to test the fixed effects model
+anova(b.fixed) 
 summary(b.fixed)
 # Use emmeans function from emmeans package to get post-hoc pairwise comparisons
 emmeans(b.fixed, pairwise ~ trt, adjust="none")
+
 
 # b Blocks random
 
 # package nlme, function lme
 
 library(nlme)
-
+# Fit a mixed model with block as random effect
 b.random1 <- lme(nitrogen ~ trt, random= ~ 1 | block, data=rcbd)
-
+# Use anova to test the mixed effects model
 anova(b.random1)
+
 summary(b.random1)
 # Use emmeans function from emmeans package to get post-hoc pairwise comparisons
 emmeans(b.random1, pairwise ~ trt, adjust="none")
@@ -36,19 +39,20 @@ emmeans(b.random1, pairwise ~ trt, adjust="none")
 
 #lmer
 library(lme4)
-
+# Fit a mixed model with block as random effect
 b.random2 <- lmer(nitrogen ~ trt + (1 | block), data=rcbd)
 summary(b.random2)
-
+# Use anova to test the mixed effects model
 anova(b.random2)
 # Notice: no P-value for F-test
-
+# This is because the lmer function does not provide P-values for F-tests by default.
 # To get P-value load lmerTest and pbkrtest
-library(lmerTest)
-library(pbkrtest)
+library(lmerTest) 
+library(pbkrtest) 
+# These packages provide methods for calculating P-values for F-tests in mixed models
 # Refit the mixed model once these packages are loaded
 b.random2 <- lmer(nitrogen ~ trt + (1 | block), data=rcbd)
-anova(b.random2, ddf="Kenward-Roger")
+anova(b.random2, ddf="Kenward-Roger") # Use Kenward-Roger method for degrees of freedom 
 
 # Use emmeans function from emmeans package to get post-hoc pairwise comparisons
 emmeans(b.random2, pairwise ~ trt, adjust="none")
